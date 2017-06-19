5b70a9f8aea8c1b94add2f623a1a479939045fbf
cjgraham95 <Orange44!>
2017-06-19 11:25:12 -0700

    MRADEV-404 Created proxy directory/ install file. MRADEV-391 Created proxy docker file.

---
+++ proxy/local/sbin/install-nginx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$USE_NGINX_PLUS" = true ];
+then
+  echo "Installing NGINX Plus"
+
+  wget -q -O /etc/ssl/nginx/CA.crt https://cs.nginx.com/static/files/CA.crt
+  wget -q -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
+  wget -q -O /etc/apt/apt.conf.d/90nginx https://cs.nginx.com/static/files/90nginx
+  printf "deb https://plus-pkgs.nginx.com/ubuntu `lsb_release -cs` nginx-plus\n" >/etc/apt/sources.list.d/nginx-plus.list
+
+  # Install NGINX Plus
+  apt-get update
+  apt-get install -o Dpkg::Options::="--force-confold" -y nginx-plus
+
+  /usr/local/sbin/generate_config -p /etc/nginx/fabric_config.yaml -t /etc/nginx/nginx-plus-fabric.conf.j2 > /etc/nginx/nginx-fabric.conf
+else
+  echo "Installing NGINX OSS"
+
+  wget -q -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
+  printf "deb http://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx\n" >/etc/apt/sources.list.d/nginx.list
+
+  apt-get update
+  apt-get install -o Dpkg::Options::="--force-confold" -y nginx
+
+  /usr/local/sbin/generate_config -p /etc/nginx/fabric_config.yaml -t /etc/nginx/nginx-fabric.conf.j2 > /etc/nginx/nginx-fabric.conf
+fi
