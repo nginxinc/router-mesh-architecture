1f32aaf18ca9664822ebff27e247db2821b60153
cjgraham95 <cjgraham95@gmail.com>
2017-06-21 15:50:16 -0700

    MRADEV-394 Started working on Web Docker. Need to look at RUN command with python MRADEV-406 install-nginx file created MRADEV-407 install-nginx file created

--- web/local/sbin/install-nginx.sh
+++ web/local/sbin/install-nginx.sh
@@ -7,21 +7,21 @@ then
   wget -q -O /etc/ssl/nginx/CA.crt https://cs.nginx.com/static/files/CA.crt
   wget -q -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
   wget -q -O /etc/apt/apt.conf.d/90nginx https://cs.nginx.com/static/files/90nginx
-  printf "deb https://plus-pkgs.nginx.com/ubuntu `lsb_release -cs` nginx-plus\n" >/etc/apt/sources.list.d/nginx-plus.list
+  printf "deb https://plus-pkgs.nginx.com/debian `lsb_release -cs` nginx-plus\n" >/etc/apt/sources.list.d/nginx-plus.list
 
   # Install NGINX Plus
   apt-get update
   apt-get install -o Dpkg::Options::="--force-confold" -y nginx-plus
 
-  /usr/local/sbin/generate_config -p /etc/nginx/fabric_config.yaml -t /etc/nginx/nginx-plus-fabric.conf.j2 > /etc/nginx/nginx-fabric.conf
+  /usr/local/sbin/generate_config -p /etc/nginx/router-mesh_config.yaml -t /etc/nginx/nginx-plus-router-mesh.conf.j2 > /etc/nginx/nginx-router-mesh.conf
 else
   echo "Installing NGINX OSS"
 
   wget -q -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
-  printf "deb http://nginx.org/packages/mainline/ubuntu/ `lsb_release -cs` nginx\n" >/etc/apt/sources.list.d/nginx.list
+  printf "deb http://nginx.org/packages/debian/ `lsb_release -cs` nginx\n" >/etc/apt/sources.list.d/nginx.list
 
   apt-get update
   apt-get install -o Dpkg::Options::="--force-confold" -y nginx
 
-  /usr/local/sbin/generate_config -p /etc/nginx/fabric_config.yaml -t /etc/nginx/nginx-fabric.conf.j2 > /etc/nginx/nginx-fabric.conf
-fi`
+  /usr/local/sbin/generate_config -p /etc/nginx/router-mesh_config.yaml -t /etc/nginx/nginx-router-mesh.conf.j2 > /etc/nginx/nginx-router-mesh.conf
+fi
