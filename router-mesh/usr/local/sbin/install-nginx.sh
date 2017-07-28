2b2f12d9725914296b83d6b46e7ec28053ef381e
cpretzer <charles.pretzer@nginx.com>
2017-07-28 11:13:38 -0700

    * MRADEV-417: create router mesh nginx instance: Dockerfile

---
+++ router-mesh/usr/local/sbin/install-nginx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+wget -q https://s3-us-west-1.amazonaws.com/fabric-model/config-generator/generate_config > /usr/local/sbin/generate_config
+chmod 755 /usr/local/sbin/generate_config
+
+if [ "$USE_NGINX_PLUS" = true ];
+then
+  echo "Installing NGINX Plus"
+
+  wget -q -O /etc/ssl/nginx/CA.crt https://cs.nginx.com/static/files/CA.crt
+  wget -q -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
+  wget -q -O /etc/apt/apt.conf.d/90nginx https://cs.nginx.com/static/files/90nginx
+  printf "deb https://plus-pkgs.nginx.com/ubuntu `lsb_release -cs` nginx-plus\n" >/etc/apt/sources.list.d/nginx-plus.list
+
+  # Install NGINX Plus
+  apt-get update
+  apt-get install -o Dpkg::Options::="--force-confold" -y nginx-plus
+
+  /usr/local/sbin/generate_config -p /etc/nginx/router-mesh_config.yaml -t /etc/nginx/nginx-plus-router-mesh.conf.j2 > /etc/nginx/nginx-router-mesh.conf
+else
+  echo "Installing NGINX OSS"
+
+  wget -q -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
+  printf "deb http://nginx.org/packages/ubuntu/ `lsb_release -cs` nginx\n" >/etc/apt/sources.list.d/nginx.list
+
+  apt-get update
+  apt-get install -o Dpkg::Options::="--force-confold" -y nginx
+
+  /usr/local/sbin/generate_config -p /etc/nginx/router-mesh_config.yaml -t /etc/nginx/nginx-router-mesh.conf.j2 > /etc/nginx/nginx-router-mesh.conf
+fi
---
+++ router-mesh/usr/local/sbin/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+pid="/var/run/nginx.pid";
+nginx_conf="/etc/nginx/nginx-router-mesh.conf";
+
+nginx -c "$nginx_conf" -g "pid $pid;" &
+echo "Started NGINX"
+
+echo launched processes;
+sleep 10;
+
+while [ -f "$pid" ]
+do
+	sleep 5;
+done
+
+echo "The start script has exited"
